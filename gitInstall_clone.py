1c12813eea4c9a9e655361ef70b6a2df63827e51
ramon <ramonestii@hotmail.com>
2019-11-22 13:04:38 -0300

    fix gitClone_6_3 function

--- gitInstall_clone.py
+++ gitInstall_clone.py
@@ -4,7 +4,7 @@ def install_git():
     os.system('apt install git')
     
 def gitClone_6_3():
-    os.system('git clone --branch dspace-6_3 --single-branch https://github.com/DSpace/DSpace.git')
+    os.system('git clone --branch dspace-6.3 --single-branch https://github.com/DSpace/DSpace.git')
 
 
 #def git-clone_7
